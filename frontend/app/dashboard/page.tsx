27474c90870140f5829d4905b46bc0ccde4fa6d2
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-12-18 20:30:35 -0500

    frontend reset

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <div className="w-1/2 h-1/2 m-auto text-center justify-center">
+        <h1>Speech Analysis</h1>
+        <h3>Get a detailed analysis</h3>
+      </div>
+    </div>
+  );
+}
